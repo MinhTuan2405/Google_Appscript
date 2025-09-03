f6e83abb75bb92570add2557e42272c680bddafa
mnhtndev <tuannguyen.02042005@gmail.com>
2025-09-03 12:33:42 +0700

    sync at 2025-09-03 12:33:32

--- EduTrack/source/Folder.js
+++ EduTrack/source/Folder.js
@@ -29,6 +29,19 @@ function getAllClasses() {
 }
 
 
+function createFolderStructure (classname, jsontr) {
+  const ss = SpreadsheetApp.getActiveSpreadsheet ()
+  let folderSheet = ss.getSheetByName ('Folder Structure')
+
+  if (!folderSheet) {
+    ss.insertSheet ('Folder Structure')
+    folderSheet = ss.getSheetByName ('Folder Structure')
+
+
+  }
+}
+
+
 /////////////////////////////////////////////
 function showChangeFolderStructureSidebar () {
   const html = HtmlService.createHtmlOutputFromFile ('changeFolderStructureUI') // ref: changeFolderStructureUI.html
